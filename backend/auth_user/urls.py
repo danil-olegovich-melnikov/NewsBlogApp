90e5013fc546154bdd15632fbc0aff1e6cd4e9f7
Danil Melinkov <danil369.ru@mail.ru>
2022-01-09 08:08:22 +0600

    JWT LOGIN + REGISTER + TOKEN UPDATE

--- backend/auth_user/urls.py
+++ backend/auth_user/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.urls import include
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
 
 
 urlpatterns = [
-
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,8 @@ MEDIA_ROOT = BASE_DIR.joinpath('media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
