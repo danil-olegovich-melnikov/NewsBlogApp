25f26016a5ce4929a0efe572d9f55b741dd20d5c
Danil Melinkov <danil369.ru@mail.ru>
2022-01-14 06:10:08 +0600

    DEBUG = TRUE

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1u5qm!38@y-jk6mls+b-gw=tn$kcyx2yd%n3wwz09q4ouk0lfv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
