f62103fc066bb448c3c95390b8710db976159dea
user <danil369.ru@mail.ru>
2022-01-19 10:06:34 +0600

    Email verification with code

--- backend/auth_user/models.py
+++ backend/auth_user/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
+from django.core.mail import send_mail
+import random
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Доп настройки")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
     picture = models.ImageField('Фотография', upload_to='auth/users/')
 
     def __str__(self):
         return self.user.username
+
+
+class UserEmailVerification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь", to_field='username')
+    code = models.PositiveSmallIntegerField('Код активации')
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        self.code = random.randint(100000, 999999)
+        send_mail(
+            subject='Subject here',
+            message=f'Verification code: {self.code}',
+            recipient_list=['danil369.ru@mail.ru'],
+            from_email=settings.EMAIL_HOST_PASSWORD,
+        )
+        super(UserEmailVerification, self).save(*args, **kwargs)
--- backend/auth_user/urls.py
+++ backend/auth_user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-
 from . import views
 
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('verification/', views.UserEmailVerificationView.as_view(), name='verification'),
 ]
